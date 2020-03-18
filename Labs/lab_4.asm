1897965aa2f7cd8041063b988182ead8d4ebf721
Melmars <49505166+melmarsezio@users.noreply.github.com>
2020-03-18 17:18:47 +0800

    Update lab_4.asm

--- Labs/lab_4.asm
+++ Labs/lab_4.asm
@@ -15,7 +15,7 @@
 .def temp2  =r18
 .equ factor = 1
 
-	jmp RESET				; interrupts vectors
+	jmp RESET		; interrupts vectors
 .org INT0addr
 	jmp EXT_INT0
 
@@ -25,7 +25,7 @@
 	rcall lcd_wait
 .endmacro
 
-.macro do_lcd_data			; sent data to LCD
+.macro do_lcd_data		; sent data to LCD
 	mov r25, @0
 	rcall lcd_data
 	rcall lcd_wait
@@ -34,16 +34,16 @@
 .macro Rem; A, B, C		; calculate remainder, A-> A%B, C-> A/B, B is unchanged
 	ldi @2, 0
 Loop:
-	cpi @0, @1			; Compare A & B
-	BRLO EndRem			; Jump to finish if A < B
-	subi @0, @1			; If A >= B: A =  A - B
-	inc @2				; Increase C
-	rjmp Loop			; Loop back
-EndRem:					; when Rem finish, A will be A%B, C will be A/B
+	cpi @0, @1		; Compare A & B
+	BRLO EndRem		; Jump to finish if A < B
+	subi @0, @1		; If A >= B: A =  A - B
+	inc @2			; Increase C
+	rjmp Loop		; Loop back
+EndRem:				; when Rem finish, A will be A%B, C will be A/B
 .endmacro
 
 .macro display_number
-	push temp1			; save temp1 and temp2 in case they still hold useful values
+	push temp1		; save temp1 and temp2 in case they still hold useful values
 	push temp2
 	mov temp1, @0
 	cpi temp1, 100
@@ -71,7 +71,7 @@ digit_2:
 	do_lcd_data temp1
 	rjmp display_finish
 display_finish:
-	pop temp2			; resume temp2 and temp1 from stack
+	pop temp2		; resume temp2 and temp1 from stack
 	pop temp1
 	rjmp display_end
 display_end:
@@ -90,10 +90,10 @@ RESET:
 
 	;reset LCD
 	ser r25
-	out DDRF, r25				; set Port A & F as output Port
+	out DDRF, r25		; set Port A & F as output Port
 	out DDRA, r25
 	clr r25
-	out PORTF, r25				; empty Port A & F
+	out PORTF, r25		; empty Port A & F
 	out PORTA, r25
 	; initialize LCD
 	do_lcd_command 0b00111000 ; 2x5x7
@@ -109,40 +109,40 @@ RESET:
 	sei
 	jmp OUTPUTLCD
 
-EXT_INT0:				; interrupt triggered when OpO has falling edge
+EXT_INT0:			; interrupt triggered when OpO has falling edge
 	push temp1
 	in temp1, SREG
 	push temp1
-	inc counter			; increase counter by 1 (has one OpO signal)
+	inc counter		; increase counter by 1 (has one OpO signal)
 	pop temp1
 	out SREG, temp1
 	pop temp1
-	reti				; return
+	reti			; return
 
-OUTPUTLCD:						; update RPS
+OUTPUTLCD:				; update RPS
 	do_lcd_command 0b00000001	; clear lcd
 	ldi temp1, factor
 	mul counter, temp1
-	mov counter, r0				; calculate RPS & store in counter
-	ldi temp1, 'R'				; print 'R'
+	mov counter, r0			; calculate RPS & store in counter
+	ldi temp1, 'R'			; print 'R'
 	do_lcd_data temp1
-	ldi temp1, 'P'				; print 'P'
+	ldi temp1, 'P'			; print 'P'
 	do_lcd_data temp1
-	ldi temp1, 'S'				; print 'S'
+	ldi temp1, 'S'			; print 'S'
 	do_lcd_data temp1
-	ldi temp1, ':'				; print ':'
+	ldi temp1, ':'			; print ':'
 	do_lcd_data temp1
 	display_number counter		; display ascii of RPS
-	clr counter					; reset counter
-	rcall sleep_250ms			; wait for 100ms (interrupts can happend during this time)
-	rjmp OUTPUTLCD				; loop back to update lcd again
+	clr counter			; reset counter
+	rcall sleep_250ms		; wait for 100ms (interrupts can happend during this time)
+	rjmp OUTPUTLCD			; loop back to update lcd again
 
 
 .equ F_CPU = 16000000
 .equ DELAY_1MS = F_CPU / 4 / 1000 - 4
 ; 4 cycles per iteration - setup/call-return overhead
 
-sleep_1ms:				; wait for 1ms
+sleep_1ms:			; wait for 1ms
 	push r24
 	push r25
 	ldi r25, high(DELAY_1MS)
@@ -154,7 +154,7 @@ delayloop_1ms:
 	pop r24
 	ret
 
-sleep_5ms:				; wait for 5ms
+sleep_5ms:			; wait for 5ms
 	rcall sleep_1ms
 	rcall sleep_1ms
 	rcall sleep_1ms
@@ -162,7 +162,7 @@ sleep_5ms:				; wait for 5ms
 	rcall sleep_1ms
 	ret
 
-sleep_25ms:				; wait for 25ms
+sleep_25ms:			; wait for 25ms
 	rcall sleep_5ms
 	rcall sleep_5ms
 	rcall sleep_5ms
@@ -228,7 +228,7 @@ lcd_data:
 	ret
 
 lcd_wait:
-	push r25			; save conflict register on stack
+	push r25		; save conflict register on stack
 	clr r25
 	out DDRF, r25		; set Port F as input port
 	out PORTF, r25
@@ -241,10 +241,10 @@ lcd_wait_loop:
     nop
 	in r25, PINF
 	lcd_clr LCD_E
-	sbrc r25, 7			; check if BF is 0
+	sbrc r25, 7		; check if BF is 0
 	rjmp lcd_wait_loop
 	lcd_clr LCD_RW
 	ser r25
 	out DDRF, r25
-	pop r25				;resume conflict register from stack
+	pop r25			;resume conflict register from stack
 	ret
